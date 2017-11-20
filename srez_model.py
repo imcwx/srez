0454cc81559a8af147420ad559bd802211509224
wenxiang <chewwenxiang@gmail.com>
2017-11-20 16:03:59 +0800

    version 1.5: debugging. dropout placeholder

--- srez_model.py
+++ srez_model.py
@@ -330,7 +330,7 @@ class Model:
         """Adds dropout function to this model"""
 
         with tf.variable_scope(self._get_layer_str()):
-            dropout = tf.placeholder(tf.float32)
+            dropout = tf.placeholder_with_default(FLAGS.dropout, name="dropout")
             out = tf.nn.dropout(self.get_output(), dropout)
             # out = tf.nn.dropout(self.get_output())
 
